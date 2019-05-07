3a91f64b88edcaa59bb8fd7b82d0e1fe50a7acca
Thales Serra <thales.serra97@gmail.com>
2019-05-07 16:05:13 -0300

    carrinho

--- resources/views/user/pages/cart.blade.php
+++ resources/views/user/pages/cart.blade.php
@@ -9,37 +9,40 @@
   <div class="content-wrapper">
     <!-- Content Header (Page header) -->
     <section class="content-header">
-      <div class="row">
-        @if(count($auth->cart) == 0)
-        <div class="col-sm-3"></div>
-        <div class="col-sm-6">
-          <h1>Não possuem cursos</h1>
-        </div>
-        <div class="col-sm-3"></div>
-        @else
         <div class="row">
-          <div class="col-sm-6">
-            @foreach(array_chunk($auth->cart->toArray() , 2) as $chunks)
-            <div class="col-sm-2">
-                @foreach($chunks as $cart)
-                <div class="column col-xs-12 col-sm-12 col-6 col-lg-6 col-xl-6 two">
-                    <div class="columns course-cart-item">
-                        <div class="column col-4 check-img"></div>  
-                        <div class="column col-5 three">
-                            <p class="course-cart-title">{{ $cart['name'] }}</p><br/>
-                            <p>{{ $cart['desc'] }}</p><br/>
-                            <p>R$ {{ $cart['price'] }}</p>                                                
-                        </div>
-                        <div class="column col-1"></div>           
-                    </div>
-                </div>
-                @endforeach
+        @if(count($auth->cart) == 0)
+            <div class="col-sm-6">
+              <h1>Não possuem cursos</h1>
             </div>
-            @endforeach                                    
-          </div>
-        </div>                                                        
+        @else
+            <div class="col-sm-6">
+                @foreach(array_chunk($auth->cart->toArray() , 2) as $chunks)
+                <div class="row">
+                    @foreach($chunks as $cart)
+                    <div class=" col-xs-3">
+                        <p><b>{{ $cart['name'] }}</b><p/>
+                        <p>{{ $cart['desc'] }}</p><br/>
+                        <p>R$ {{ $cart['price'] }}</p>                                                
+                    </div>
+                    @endforeach
+                </div>
+                @endforeach                                    
+            </div>
+            <div class="col-sm-6">
+                <div class="box-w-shadow">
+                    <h1>Subtotal</h1>
+                    <h1>R$ {{number_format($auth->cart->sum('price'), 2, ',', '.')}}</h1>
+                    <form>
+                        <input type="text" name="coupon">
+                    </form>
+                    <h3>Total</h3>
+                    <h3>R$ {{number_format($auth->cart->sum('price'), 2, ',', '.')}}</h3>
+                    <button>Finalizar Compra</button>
+                </div>
+            </div>                                           
         @endif
-      </div>
+
+        </div>
     </section>
   </div>
 
