3d4a163f119daf64a0319cda540978126961819e
Nego Thales <thale.serra97@gmail.com>
2019-08-05 10:48:55 -0300

    tag

--- resources/views/admin/pages/blog/post/edit.blade.php
+++ resources/views/admin/pages/blog/post/edit.blade.php
@@ -177,7 +177,8 @@
                     {{csrf_field()}}
 
                     <div class="box-body">
-                    <div class="form-group" data-select2-id="24">
+                    <div class="col-md-6">
+              <div class="form-group" data-select2-id="24">
                 <label>Multiple</label>
                 <select class="form-control select2 select2-hidden-accessible" multiple="" data-placeholder="Select a State" style="width: 100%;" data-select2-id="7" tabindex="-1" aria-hidden="true">
                   <option data-select2-id="25">Alabama</option>
@@ -187,8 +188,23 @@
                   <option data-select2-id="29">Tennessee</option>
                   <option data-select2-id="30">Texas</option>
                   <option data-select2-id="31">Washington</option>
-                </select><span class="select2 select2-container select2-container--default select2-container--below" dir="ltr" data-select2-id="8" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--multiple" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="-1"><ul class="select2-selection__rendered"><li class="select2-search select2-search--inline"><input class="select2-search__field" type="search" tabindex="0" autocomplete="off" autocorrect="off" autocapitalize="none" spellcheck="false" role="textbox" aria-autocomplete="list" placeholder="Select a State" style="width: 517.5px;"></li></ul></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+                </select><span class="select2 select2-container select2-container--default select2-container--below" dir="ltr" data-select2-id="8" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--multiple" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="-1"><ul class="select2-selection__rendered"><li class="select2-selection__choice" title="California" data-select2-id="46"><span class="select2-selection__choice__remove" role="presentation">×</span>California</li><li class="select2-selection__choice" title="Tennessee" data-select2-id="47"><span class="select2-selection__choice__remove" role="presentation">×</span>Tennessee</li><li class="select2-search select2-search--inline"><input class="select2-search__field" type="search" tabindex="0" autocomplete="off" autocorrect="off" autocapitalize="none" spellcheck="false" role="textbox" aria-autocomplete="list" placeholder="" style="width: 0.75em;"></li></ul></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
               </div>
+              <!-- /.form-group -->
+              <div class="form-group" data-select2-id="13">
+                <label>Disabled Result</label>
+                <select class="form-control select2 select2-hidden-accessible" style="width: 100%;" data-select2-id="9" tabindex="-1" aria-hidden="true">
+                  <option selected="selected" data-select2-id="11">Alabama</option>
+                  <option data-select2-id="17">Alaska</option>
+                  <option disabled="disabled" data-select2-id="18">California (disabled)</option>
+                  <option data-select2-id="19">Delaware</option>
+                  <option data-select2-id="20">Tennessee</option>
+                  <option data-select2-id="21">Texas</option>
+                  <option data-select2-id="22">Washington</option>
+                </select><span class="select2 select2-container select2-container--default select2-container--below" dir="ltr" data-select2-id="10" style="width: 100%;"><span class="selection"><span class="select2-selection select2-selection--single" role="combobox" aria-haspopup="true" aria-expanded="false" tabindex="0" aria-labelledby="select2-b51a-container"><span class="select2-selection__rendered" id="select2-b51a-container" role="textbox" aria-readonly="true" title="Alabama">Alabama</span><span class="select2-selection__arrow" role="presentation"><b role="presentation"></b></span></span></span><span class="dropdown-wrapper" aria-hidden="true"></span></span>
+              </div>
+              <!-- /.form-group -->
+            </div>
                     </div>
                 </div>
                 <div class="modal-footer">
