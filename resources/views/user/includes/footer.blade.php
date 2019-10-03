db17f4c4d703f535b34b2b92664c24f8b50a9d2e
Nego Thales <thale.serra97@gmail.com>
2019-10-03 14:48:01 -0300

    carrousel relacionados

--- resources/views/user/includes/footer.blade.php
+++ resources/views/user/includes/footer.blade.php
@@ -229,6 +229,42 @@ $('#destaque2-carousel').slick({
     ]
 });
 
+$('#recommended-carousel').slick({
+    dots: false,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    prevArrow: $('#recommended-prev'),
+    nextArrow: $('#recommended-next'),
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+    ]
+});
+
+
 $(document).on('click', '.scroll', function(event){
     var tela = $(window).width();
     if(tela < 768){
